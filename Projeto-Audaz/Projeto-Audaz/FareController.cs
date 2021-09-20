1cda356870b77a066ca2979ded224f8b93ad348d
NScorzelli <89496417+NScorzelli@users.noreply.github.com>
2021-09-20 02:49:19 -0300

    Projeto Audaz

---
+++ Projeto-Audaz/Projeto-Audaz/FareController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestePleno.Models;
+using TestePleno.Services;
+
+namespace TestePleno.Controllers
+{
+    public class FareController
+    {
+        private OperatorService OperatorService;
+        private FareService FareService;
+
+        public FareController()
+        {
+            OperatorService = new OperatorService();
+            FareService = new FareService();
+        }
+
+        public void CreateFare(Fare fare, string operatorCode)
+        {
+
+            var selectedOperator = OperatorService.GetOperatorByCode(operatorCode);
+
+            if (selectedOperator == null)
+            {
+                var _operator = new Operator();
+                _operator.Id = Guid.NewGuid();
+                _operator.Code = operatorCode;
+                OperatorService.Create(_operator);
+                selectedOperator = _operator;
+            }
+            else
+            {
+
+                if (FareService.FareValidator(fare.Value, selectedOperator.Id)) 
+                {
+                    throw new Exception($"Já existe um registro para a entidade '{fare.Value}' com o Operadora '{selectedOperator.Code}' no período de 6 meses.");
+                }
+
+            }
+
+            fare.OperatorId = selectedOperator.Id;
+            FareService.Create(fare);
+
+        }
+    }
+}
---
+++ Projeto-Audaz/Projeto-Audaz/FareService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestePleno.Models;
+
+namespace TestePleno.Services
+{
+    public class FareService
+    {
+        private Repository _repository = new Repository();
+
+        public void Create(Fare fare)
+        {
+            _repository.Insert(fare);
+        }
+
+        public void Update(Fare fare)
+        {
+            _repository.Update(fare);
+        }
+
+        public Fare GetFareById(Guid fareId)
+        {
+            var fare = _repository.GetById<Fare>(fareId);
+            return fare;
+        }
+
+        public List<Fare> GetFares()
+        {
+            var fares = _repository.GetAll<Fare>();
+            return fares;
+        }
+
+        public Boolean FareValidator(decimal value, Guid operatorId)
+        {
+            var fares = _repository.GetAll<Fare>();
+             return fares.Any(selectedFares => selectedFares.OperatorId == operatorId && selectedFares.Value == value && selectedFares.Status == 1 && (selectedFares.InsertedDate >= DateTime.Now.AddMonths(-6)));
+        }
+    }
+}
